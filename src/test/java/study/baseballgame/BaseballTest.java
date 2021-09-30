a8a0f105201c90a20ebf40ede4112ff69204abaf
Seokho-Ham <seokho9522@gmail.com>
2021-09-30 12:45:09 +0900

    Baseball 테스트,프로덕션 코드 작성

--- src/test/java/study/baseballgame/BaseballTest.java
+++ src/test/java/study/baseballgame/BaseballTest.java
@@ -1,4 +1,16 @@
 package study.baseballgame;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import com.baseballgame.Baseball;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class BaseballTest {
+
+    @Test
+    @DisplayName("입력받은 값으로 baseball 객체를 생성하는 테스트")
+    void baseball() {
+        Baseball ball = new Baseball("123");
+        assertThat(ball.getBall()).isEqualTo("123");
+    }
 }
